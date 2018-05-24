c2b18b423a00e7e3493be75fe63a4c2ffc041183
viviana <dmpviviana@gmail.com>
2018-05-24 09:41:59 -0400

    agregando parametros en funcion encode

--- cipher.js
+++ cipher.js
@@ -1,6 +1,17 @@
 window.cipher = {
-  encode: () => {
+
+  encode: (offset,string) => {
     /* Acá va tu código */
+    offset = 33;
+    string = document.getElementById('cipher').value;
+    const cifradas = '';
+    console.log(string);
+    for (let i = 0; i < string.length; i++) {
+      const element = string[i];
+      let formula = (string.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
+      let cifrado = String.fromCharCode(formula);
+      console.log(element)
+    }
   },
   decode: () => {
     /* Acá va tu código */
