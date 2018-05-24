5ce2d9337e39624a2afd1b0a43a533125b0693e9
viviana <dmpviviana@gmail.com>
2018-05-24 14:02:19 -0400

    decode listo

--- cipher.js
+++ cipher.js
@@ -16,5 +16,15 @@ window.cipher = {
   },
   decode: () => {
     /* Acá va tu código */
+    let textDe = document.getElementById('cipher').value;
+    offset = 33;
+    string = textDe;
+    let cifradasDecode = '';
+    for (let j = 0; j < string.length; j++) {
+      let formula = (string.toUpperCase().charCodeAt(j) + 65 - offset) % 26 + 65;
+      let cifrado = String.fromCharCode(formula);
+      cifradasDecode+= cifrado;
   }
+  document.getElementById('containerDC').innerHTML = '<p>' + cifradasDecode + '</p>';
+}
 }
