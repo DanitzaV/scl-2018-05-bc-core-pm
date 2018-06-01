c0861829c7cd6f7e30189e463a78a6fcf9ed2363
viviana <dmpviviana@gmail.com>
2018-05-31 21:50:44 -0400

    cambio en asignacion de variable, espacios en descifrado

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ let offset1 = document.getElementById('number');
 function cifrar() {
     document.getElementById('containerCC').innerHTML = window.cipher.encode(string1.value,parseInt(offset1.value));
 }
+function decode() {
+    document.getElementById('containerCC').innerHTML = window.cipher.encode(string1.value,parseInt(offset1.value));
+}
