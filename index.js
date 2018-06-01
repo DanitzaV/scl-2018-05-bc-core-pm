32ba83293f41d41a39173ab3a1a17592005dc020
viviana <dmpviviana@gmail.com>
2018-05-31 21:28:16 -0400

    primer intento DOM en index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-/* Acá va tu código */
+
+let string1 = document.getElementById('cipher').value;
+let offset1 = document.getElementById('number').value;
+
+document.getElementById("cifrar").addEventListener('click', function(){
+    document.getElementById('containerCC').innerHTML = window.cipher.encode(string1.toUpperCase(),parseInt(offset1));
+})
